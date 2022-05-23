7d41fdb179105b8c4b589adc24813409bc9d6a2d
Pauluci <pauluci125romao.pedro@hotmail.com>
2022-05-23 08:43:54 -0300

    Update: creating routes and children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { MomentsComponent } from './components/pages/moments/moments.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'moments', component: MomentsComponent,
+  children:[
+    {
+      path: 'new', component: NewMomentComponent
+    }
+  ]
+},
 ];
 
 @NgModule({
