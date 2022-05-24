8c0b5a678917089a533e3e6af59736bd4a27f96e
Pauluci <pauluci125romao.pedro@hotmail.com>
2022-05-24 08:48:52 -0300

    Update: finalizing registration

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -52,17 +52,18 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get('description')!;
   }
 
-  onFileSelected(event: any){
+  fileChangeEvent(event: any){
 
-    const file: File = event.target.files[0]
+    alert("tem imagem");
 
-    this.momentForm.patchValue({image: file})
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
 
   }
 
   submit() {
     if (this.momentForm.invalid) {
-      alert("Erro ao enviar o formulario");
       return;
     }
 
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/interfaces/Moments';
 import { MomentService } from 'src/app/services/moment.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -9,23 +10,28 @@ import { MomentService } from 'src/app/services/moment.service';
 })
 export class NewMomentComponent implements OnInit {
   btnText = "Salvar Momento";
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
-   async createHandler(moment: Moment) {
+  async createHandler(moment: Moment){
     const formData = new FormData()
 
-    formData.append("name", moment.name)
-    formData.append("email", moment.email)
-    formData.append("data", moment.data)
-    formData.append("title", moment.title)
-    formData.append("description", moment.description)
+    formData.append('name', moment.name);
+    formData.append('email', moment.email);
+    formData.append('data', moment.data);
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
 
-    if (moment.image) {
-      formData.append("image", moment.image);
+    if(moment.image){
+      formData.append('image', moment.image);
     }
+
     await this.momentService.createMoment(formData).subscribe();
+
   }
 }
