ff0245094b07ea5ce46b6a878b7e1dbedf7be327
Pauluci <pauluci125romao.pedro@hotmail.com>
2022-05-25 11:45:15 -0300

    Update: create delete a moment

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -3,6 +3,7 @@ import { MomentService } from 'src/app/services/moment.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Moment } from 'src/app/interfaces/Moments';
 import { environment } from 'src/environments/environment';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-moment',
@@ -14,12 +15,25 @@ export class MomentComponent implements OnInit {
   moment?: Moment;
   baseApiUrl = environment.baseApiUrl;
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+    private alertService: AlertService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data));
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe()
+
+    this.alertService.success('Sucesso', 'Momento Deletado com sucesso!');
+
+    this.router.navigate(['moments'])
   }
 
 }
